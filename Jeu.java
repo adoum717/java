ffd6f34163aa5aa57429d6aa9982120557416844
Idriss Adoum Idriss <56471997+adoum717@users.noreply.github.com>
2019-10-27 17:13:03 +0100

    Create Jeu.java

---
+++ Jeu.java
@@ -0,0 +1,10 @@
+import java.utilisé.Scanner;
+public class Jeu{
+static Scanner clavier = new Scanner (Système.in)
+   public static main void (String[] arcs) {
+   
+  int nombre_user, nombre_secret;
+  Système.out.println("Saisir un nombre : (1-50) ");
+  nombre_user = clavier.nextInt();
+ }
+}
